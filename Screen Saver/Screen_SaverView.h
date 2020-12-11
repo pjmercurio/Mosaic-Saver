572ede8c975d9cf6e825db80c5ef07de30eee9b6
Paul Mercurio <paul@frame.io>
2020-12-11 15:11:47 -0500

    Initial Commit

---
+++ Screen Saver/Screen_SaverView.h
@@ -0,0 +1,13 @@
+//
+//  Screen_SaverView.h
+//  Screen Saver
+//
+//  Created by Paul Mercurio on 12/11/20.
+//  Copyright © 2020 Paul Mercurio. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface Screen_SaverView : ScreenSaverView
+
+@end
---
+++ Screen Saver/Screen_SaverView.m
@@ -0,0 +1,52 @@
+//
+//  Screen_SaverView.m
+//  Screen Saver
+//
+//  Created by Paul Mercurio on 12/11/20.
+//  Copyright © 2020 Paul Mercurio. All rights reserved.
+//
+
+#import "Screen_SaverView.h"
+
+@implementation Screen_SaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
